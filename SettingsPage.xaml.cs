be5f87cb7f1254d397c3b15ad12129047fe2696f
Nicholas Cutler <91292435+steelscape02@users.noreply.github.com>
2025-03-07 14:11:19 -0700

    Method documentation

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -9,6 +9,7 @@ namespace JsonConverter
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        //create instance of storage manager for this page
         private readonly StorageManager manager;
         public SettingsPage()
         {
@@ -19,12 +20,22 @@ namespace JsonConverter
                 manager.Set(TextResources.rootName, TextResources.baseName);
         }
 
+        /// <summary>
+        /// Backbutton event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             ArgumentNullException.ThrowIfNull(e);
             _ = Frame.Navigate(typeof(MainPage));
         }
 
+        /// <summary>
+        /// Help button event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void Help_Click(object sender, RoutedEventArgs e)
         {
             if (!allOptional_TeachingTip.IsOpen && !suggestCorrs_TeachingTip.IsOpen && !validateMsgs_TeachingTip.IsOpen)
@@ -41,45 +52,82 @@ namespace JsonConverter
             }
         }
 
+        /// <summary>
+        /// Base name (Root class) change event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void BaseName_Change(object sender, TextChangedEventArgs e)
         {
             ArgumentNullException.ThrowIfNull(e);
             manager.Set(TextResources.rootName, RootName.Text);
         }
 
-        //All optional
+        /// <summary>
+        /// All Optional <b>checked</b> event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void AllOptional_Checked(object sender, RoutedEventArgs e)
         {
             manager.Set(TextResources.allOptional, true);
         }
 
+        /// <summary>
+        /// All Optional <b>unchecked</b> event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void AllOptional_Unchecked(object sender, RoutedEventArgs e)
         {
             manager.Set(TextResources.allOptional, false);
         }
 
-        //Suggest corrections
+        /// <summary>
+        /// Suggest corrections <b>checked</b> event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void SuggestCorrs_Checked(object sender, RoutedEventArgs e)
         {
             manager.Set(TextResources.suggestCorrs, true);
         }
 
+        /// <summary>
+        /// Suggest corrections <b>unchecked</b> event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void SuggestCorrs_Unchecked(object sender, RoutedEventArgs e)
         {
             manager.Set(TextResources.suggestCorrs, false);
         }
 
-        //Validation Messages
+        /// <summary>
+        /// Validation messages <b>checked</b> event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void ValidateMsgs_Checked(object sender, RoutedEventArgs e)
         {
             manager.Set(TextResources.validateMsgs, true);
         }
 
+        /// <summary>
+        /// Validation messages <b>unchecked</b> event handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void ValidateMsgs_Unchecked(object sender, RoutedEventArgs e)
         {
             manager.Set(TextResources.validateMsgs, false);
         }
 
+        /// <summary>
+        /// Options grid loaded handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void Options_Loaded(object sender, RoutedEventArgs e)
         {
             RootName.Text = manager.Get(TextResources.rootName) as string;
@@ -88,6 +136,11 @@ namespace JsonConverter
             validateMsgs.IsChecked = manager.Get(TextResources.validateMsgs) as bool? ?? false;
         }
 
+        /// <summary>
+        /// Options grid unloaded handler
+        /// </summary>
+        /// <param name="sender">The object that raised this event</param>
+        /// <param name="e">Additional event information</param>
         private void Options_Unloaded(object sender, RoutedEventArgs e)
         {
             manager.Set(TextResources.rootName, RootName.Text);
